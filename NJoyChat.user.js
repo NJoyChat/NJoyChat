593847bc337d01e58c86c2c13de3f1037201a260
NJoyChat <NJoyChat@proton.me>
2023-07-26 18:50:52 +0200

    Version bump.

--- NJoyChat.user.js
+++ NJoyChat.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NJoyChat
 // @namespace    https://www.joyclub.de/chat/login/
-// @version      Alpha-v15
+// @version      Alpha-v16
 // @description  Improves JoyChat with additional utilities.
 // @author       NJoyChat Team
 // @match        https://www.joyclub.de/chat/login/
